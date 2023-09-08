a6d2df6e1f1ccc2b2547a097fe5a6ee5b87c399b
Cameron Thomson <cameron.thomson@brightsparklabs.com>
2023-09-08 17:02:49 +1000

    APED-43: Set default run schedule to 1 min

--- src/main/java/com/joeyfrazee/nifi/reporting/ElasticsearchProvenanceReporter.java
+++ src/main/java/com/joeyfrazee/nifi/reporting/ElasticsearchProvenanceReporter.java
@@ -26,12 +26,14 @@ import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.nifi.annotation.configuration.DefaultSchedule;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.reporting.ReportingContext;
 
+import org.apache.nifi.scheduling.SchedulingStrategy;
 import org.elasticsearch.client.RestClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
@@ -40,6 +42,7 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 
 @Tags({"elasticsearch", "provenance"})
+@DefaultSchedule(strategy = SchedulingStrategy.TIMER_DRIVEN, period = "1 min")
 @CapabilityDescription("A provenance reporting task that writes to Elasticsearch")
 public class ElasticsearchProvenanceReporter extends AbstractProvenanceReporter {
     // -------------------------------------------------------------------------
